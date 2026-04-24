b2e9749fdee6284ddf00d3511cc8b5205b891f95
Nicolas Pope <nicolas.pope@uef.fi>
2026-04-24 04:30:18 +0000

    Merged PR 1251: Remove unwanted colours from theme

--- lib/style/theme.ts
+++ lib/style/theme.ts
@@ -8,28 +8,18 @@ export const theme = createTheme({
     palette: {
         primary: {
             main: isTest ? '#fff' : colours.primary,
-            light: isTest ? '#fff' : colours.primary,
-            dark: isTest ? '#fff' : colours.primaryLight,
         },
         secondary: {
             main: isTest ? '#fff' : colours.secondary,
-            light: isTest ? '#fff' : colours.secondary,
-            dark: isTest ? '#fff' : colours.secondaryLight,
         },
         success: {
             main: '#4caf50',
-            light: '#4caf50',
-            dark: '#81c784',
         },
         info: {
             main: '#75a4e2',
-            light: '#75a4e2',
-            dark: '#a4bfe2',
         },
         error: {
             main: '#f44336',
-            light: '#f44336',
-            dark: '#ED9C9C',
         },
     },
     typography: {
@@ -54,28 +44,18 @@ export const darkTheme = createTheme({
         mode: 'dark',
         primary: {
             main: isTest ? '#fff' : colours.primaryLight,
-            light: isTest ? '#fff' : colours.primary,
-            dark: isTest ? '#fff' : colours.primaryLight,
         },
         secondary: {
             main: isTest ? '#fff' : colours.secondaryLight,
-            light: isTest ? '#fff' : colours.secondary,
-            dark: isTest ? '#fff' : colours.secondaryLight,
         },
         success: {
             main: '#81c784',
-            light: '#4caf50',
-            dark: '#81c784',
         },
         info: {
             main: '#a4bfe2',
-            light: '#75a4e2',
-            dark: '#a4bfe2',
         },
         error: {
             main: '#ED9C9C',
-            light: '#f44336',
-            dark: '#ED9C9C',
         },
     },
     typography: {
